3ba306d4bc92629320505c7ea4790ac45cdcb792
test295 <62980044+test295@users.noreply.github.com>
2021-03-31 20:29:44 +0100

    Update

--- TProject/src/GUI/FInterface.java
+++ TProject/src/GUI/FInterface.java
@@ -1,20 +1,23 @@
 package GUI;
 
 import DBConnect.*;
+import ReportGenerator.generateReport;
 import com.toedter.calendar.JDateChooser;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.text.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class FInterface extends MainInterface {
 
     private String[] colHeads={"Cust Name","Contact Name","Email","Address","City","PostCode","Phone Number","Discount Type"};
     private String[][]data={{"","","","","","","",""}};
-    JButton job,pay,logout;
+    JButton job,Payment,logout;
 
     public FInterface(String path){
         super(path);
@@ -26,7 +29,7 @@ public class FInterface extends MainInterface {
         remove(buttPane);
         exTask.setText("Create new Customer");
         job = new JButton("Create Job");
-        pay = new JButton("New Payment");
+        Payment = new JButton("Payment");
         logout = new JButton("Logout");
 
         exTask.addActionListener(new ActionListener() {
@@ -66,12 +69,6 @@ public class FInterface extends MainInterface {
                     nTask.add(fields[4]);
                     nTask.add(fields[5]);
                     nTask.add(fields[6]);
-
-                    /*String[] discountRates = {"Flexible","Variable","Fixed"};
-                    JComboBox discount = new JComboBox(discountRates);
-                    discount.setBounds(105,221,150,20);
-                    nTask.add(discount);*/
-
 
                     JButton ok = new JButton("Submit");
                     ok.setToolTipText("Click to submit task info");
@@ -152,7 +149,7 @@ public class FInterface extends MainInterface {
                 date.setBounds(130,41,150,20);
                 nTask.add(date);
 
-                fields[1].setBounds(130,101,150,20);
+                fields[1].setBounds(130,71,150,20);
                 nTask.add(fields[0]);
                 nTask.add(fields[1]);
 
@@ -185,7 +182,7 @@ public class FInterface extends MainInterface {
                         DBConnection connection = new DBConnection();
                         String SQL = "SELECT Customer_Name FROM Customer WHERE Customer.Customer_Name == '" +Cust+"';";
                         String custID = connection.CustReturn(SQL);
-                        if(!custID.equals("")) {
+                        if(!custID.equals(null)) {
                             DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
                             String sql = "INSERT INTO Job (Priority,Status,Deadline,Special_instruction,CustomerCustomer_No)\n" +
                                     "VALUES(" + fields[0].getText() + ",'Pending','" + dateFormat.format(date.getDate()) + "','" + fields[1].getText() + "','" + custID + "');";
@@ -201,35 +198,26 @@ public class FInterface extends MainInterface {
             }
         });
 
-        pay.addActionListener(new ActionListener() {
+        Payment.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 remove(tPane);
                 JPanel nTask = new JPanel();
                 nTask.setLayout(null);
 
-                JLabel[] labels = {new JLabel("Amount:"),new JLabel("Payment Type:")};
-                labels[0].setBounds(5,-30,100,100);
-                labels[1].setBounds(5,0,100,100);
-                nTask.add(labels[0]);
-                nTask.add(labels[1]);
+                JButton npay = new JButton("New Payment");
+                npay.setToolTipText("Click to create new payment for new customer");
+                npay.setBounds(10,10,135,30);
+                nTask.add(npay);
 
-                JTextField[] fields = {new JTextField(10)};
-                fields[0].setBounds(130,11,150,20);
-                String[] pType = {"Cash","Card"};
-                JComboBox pay = new JComboBox(pType);
-                pay.setBounds(130,41,150,20);
-                nTask.add(fields[0]);
-                nTask.add(pay);
-
-                JButton ok = new JButton("Submit");
-                ok.setToolTipText("Click to submit task info");
-                ok.setBounds(10,250,100,30);
-                nTask.add(ok);
+                JButton UpCard = new JButton("Update Card Information");
+                UpCard.setToolTipText("Click to update customer card information");
+                UpCard.setBounds(10,50,210,30);
+                nTask.add(UpCard);
 
                 JButton cancel = new JButton("Cancel");
                 cancel.setToolTipText("Click to cancel submission");
-                cancel.setBounds(130,250,100,30);
+                cancel.setBounds(10,90,100,30);
                 nTask.add(cancel);
 
                 cancel.addActionListener(new ActionListener() {
@@ -238,55 +226,129 @@ public class FInterface extends MainInterface {
                         remove(tPane);
                         table = new JTable(data,colHeads);
                         tPane = new JScrollPane(table);
-                        table.setSize(900,500);
-                        add(tPane, BorderLayout.CENTER);
+                        tPane.setPreferredSize(new Dimension(800,500));
+                        add(tPane,BorderLayout.CENTER);
                         setVisible(true);
                     }
                 });
-                ok.addActionListener(new ActionListener() {
+
+                npay.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
+                        remove(tPane);
+                        JPanel nTask = new JPanel();
+                        nTask.setLayout(null);
 
+                        JLabel[] labels = {new JLabel("Amount:"),new JLabel("Payment Type:")};
+                        labels[0].setBounds(5,-30,100,100);
+                        labels[1].setBounds(5,0,100,100);
+                        nTask.add(labels[0]);
+                        nTask.add(labels[1]);
+
+                        JTextField[] fields = {new JTextField(10)};
+                        fields[0].setBounds(130,11,150,20);
+                        String[] pType = {"Cash","Card"};
+                        JComboBox pay = new JComboBox(pType);
+                        pay.setBounds(130,41,150,20);
+                        nTask.add(fields[0]);
+                        nTask.add(pay);
+
+                        JButton ok = new JButton("Submit");
+                        ok.setToolTipText("Click to submit task info");
+                        ok.setBounds(10,250,100,30);
+                        nTask.add(ok);
+
+                        JButton cancel = new JButton("Cancel");
+                        cancel.setToolTipText("Click to cancel submission");
+                        cancel.setBounds(130,250,100,30);
+                        nTask.add(cancel);
+
+                        cancel.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                remove(tPane);
+                                table = new JTable(data,colHeads);
+                                tPane = new JScrollPane(table);
+                                table.setSize(900,500);
+                                add(tPane, BorderLayout.CENTER);
+                                setVisible(true);
+                            }
+                        });
+                        ok.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                boolean nCust = true;
+                                DBConnection con = new DBConnection();
+                                String CustName = JOptionPane.showInputDialog(null,"Enter Customer Making Payment: ");
+                                String SQL = "SELECT Customer_ID FROM Customer WHERE Customer.Customer_Name == '" +CustName+"';";
+                                int custID = Integer.parseInt(con.CustReturn(SQL));
+                                String NewCust = "SELECT CardHolder_Name FROM Card_Info, Customer WHERE Card_Info.CustomerCustomer_ID == Customer.Customer_ID AND Customer.Customer_ID = "+custID+";";
+                                String NCard = con.CustReturn(NewCust);
+                                if(!NCard.equals("")){
+                                    nCust = false;
+                                    String card = "SELECT Card_Number FROM Card_Info, Customer WHERE Card_Info.CustomerCustomer_ID == Customer.Customer_ID AND Customer.Customer_ID = "+custID+";";
+                                    long CardNo = Long.parseLong(con.CustReturn(card));
+                                    if(custID != 0){
+                                        String sql = "INSERT INTO Payment (Amount, Payment_Type, CustomerCustomer_No, Payment_Status, Card_No)" +
+                                                "VALUES('"+fields[0].getText()+"','"+String.valueOf(pay.getSelectedItem())+"', "+custID+",FALSE,"+CardNo+");";
+                                        con.Execute(sql);
+                                    }
+                                }
+                                if(String.valueOf(pay.getSelectedItem()).equals("Card") && nCust){
+                                    if(custID != 0){
+                                        String sql = "INSERT INTO Payment (Amount, Payment_Type, CustomerCustomer_No, Payment_Status)" +
+                                                "VALUES('"+fields[0].getText()+"','"+String.valueOf(pay.getSelectedItem())+"', "+custID+",FALSE);";
+                                        con.Execute(sql);
+                                    }
+                                    CardInfo(custID,true);
+                                }
+                            }
+                        });
+
+                        tPane = new JScrollPane(nTask);
+                        table.setSize(900,500);
+                        add(tPane,BorderLayout.CENTER);
+                        setVisible(true);
+                    }
+                });
+
+                UpCard.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
                         DBConnection con = new DBConnection();
                         String CustName = JOptionPane.showInputDialog(null,"Enter Customer Making Payment: ");
                         String SQL = "SELECT Customer_ID FROM Customer WHERE Customer.Customer_Name == '" +CustName+"';";
-                        String custIDS = con.CustReturn(SQL);
-                        int custID = Integer.parseInt(custIDS);
-                        if(custID != 0){
-                            String sql = "INSERT INTO Payment (Amount, Payment_Type, CustomerCustomer_No, Payment_Status)" +
-                                    "VALUES('"+fields[0].getText()+"','"+String.valueOf(pay.getSelectedItem())+"', "+custID+",FALSE);";
-                            con.Execute(sql);
-                        }
-                        if(String.valueOf(pay.getSelectedItem()).equals("Card")){
-                            CardInfo(custID);
-                        }
+                        int custID = Integer.parseInt(con.CustReturn(SQL));
+                        CardInfo(custID,false);
                     }
                 });
 
                 tPane = new JScrollPane(nTask);
-                table.setSize(900,500);
+                tPane.setPreferredSize(new Dimension(800,500));
                 add(tPane,BorderLayout.CENTER);
                 setVisible(true);
             }
         });
 
+
+
         buttPane = new JPanel(new GridLayout(2,2));
         buttPane.add(exTask);
         buttPane.add(job);
-        buttPane.add(pay);
+        buttPane.add(Payment);
         buttPane.add(logout);
         add(buttPane, BorderLayout.SOUTH);
         setVisible(true);
     }
 
-    public void CardInfo(int custID){
+    public void CardInfo(int custID,boolean type){
         remove(tPane);
         JPanel nTask = new JPanel();
         nTask.setLayout(null);
         JLabel[] labels = {new JLabel("Card Number:"),new JLabel("Card Type:"),new JLabel("Card Holder Name:"),new JLabel("Expiry Date:"),new JLabel("Last 4 Digits:"),new JLabel("CVV:")};
         labels[0].setBounds(5,-30,100,100);
         labels[1].setBounds(5,0,100,100);
-        labels[2].setBounds(5,30,100,100);
+        labels[2].setBounds(5,30,120,100);
         labels[3].setBounds(5,60,100,100);
         labels[4].setBounds(5,90,100,100);
         labels[5].setBounds(5,120,100,100);
@@ -298,15 +360,15 @@ public class FInterface extends MainInterface {
         nTask.add(labels[5]);
 
         JTextField[] fields = {new JTextField(10),new JTextField(10),new JTextField(10),new JTextField(10)};
-        fields[0].setBounds(105,11,150,20);
-        fields[1].setBounds(105,41,150,20);
-        fields[2].setBounds(105,71,150,20);
+        fields[0].setBounds(125,11,150,20);
+        fields[1].setBounds(125,41,150,20);
+        fields[2].setBounds(125,71,150,20);
         JDateChooser exDate = new JDateChooser();
-        exDate.setBounds(105,101,150,20);
+        exDate.setBounds(125,101,150,20);
         exDate.setDateFormatString("MM/YY");
-        fields[3].setBounds(105,131,150,20);
+        fields[3].setBounds(125,131,150,20);
         JPasswordField CVV = new JPasswordField(10);
-        CVV.setBounds(105,161,150,20);
+        CVV.setBounds(125,161,150,20);
         nTask.add(fields[0]);
         nTask.add(fields[1]);
         nTask.add(fields[2]);
@@ -327,10 +389,15 @@ public class FInterface extends MainInterface {
         cancel.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                if(type == true) {
+                    DBConnection con = new DBConnection();
+                    String SQL = "DELETE FROM Payment WHERE CustomerCustomer_No =" + custID;
+                    con.Execute(SQL);
+                }
                 remove(tPane);
-                table = new JTable(data,colHeads);
+                table = new JTable(data, colHeads);
                 tPane = new JScrollPane(table);
-                table.setSize(900,500);
+                table.setSize(900, 500);
                 add(tPane, BorderLayout.CENTER);
                 setVisible(true);
             }
@@ -340,16 +407,32 @@ public class FInterface extends MainInterface {
             @Override
             public void actionPerformed(ActionEvent e) {
                 DBConnection con = new DBConnection();
-                String SQL = "SELECT Payment_ID FROM Payment, Customer " +
-                        "WHERE Customer.Customer_ID == "+custID+
-                        "AND Payment.CustomerCustomer_ID == Customer.Customer_ID;";
-                String temp = con.CustReturn(SQL);
-                int payID = Integer.parseInt(temp);
-                if(payID != 0){
-                    DateFormat dateFormat = new SimpleDateFormat("MM-yy");
-                    String sql = "INSERT INTO Card_Info VALUES("+Integer.parseInt(fields[0].getText())+",'"+fields[1].getText()+"','"+ fields[2].getText()+"','"+dateFormat.format(exDate.getDate())+"',"+Integer.parseInt(fields[3].getText())+","+Integer.parseInt(CVV.getText())+");";
+                DateFormat dateFormat = new SimpleDateFormat("MM-yy");
+                if(type == true) {
+                    String SQL = "SELECT Payment_ID FROM Payment, Customer\n" +
+                            "WHERE Payment.CustomerCustomer_No == Customer.Customer_ID \n" +
+                            "AND Customer.Customer_ID ==" + custID + "\n" +
+                            "ORDER BY Payment_ID DESC LIMIT 1;";
+                    String temp = con.CustReturn(SQL);
+                    int payID = Integer.parseInt(temp);
+                    if (payID != 0) {
+                        String sql = "INSERT INTO Card_Info VALUES(" + Long.parseLong(fields[0].getText()) + ",'" + fields[1].getText() + "','" + fields[2].getText() + "','" + dateFormat.format(exDate.getDate()) + "'," + Integer.parseInt(fields[3].getText()) + "," + Integer.parseInt(CVV.getText()) + "," + custID + ");";
+                        con.Execute(sql);
+                        String update = "UPDATE Payment SET Card_No = " + Long.parseLong(fields[0].getText()) + " WHERE Payment.Payment_ID == " + payID + ";";
+                        con.Execute(update);
+                    }
+                }else{
+                    String sql = "UPDATE Card_Info \n" +
+                            "SET Card_Number = "+Long.parseLong(fields[0].getText()) +", Card_Type = '"+fields[1].getText()+"', CardHolder_Name = '"+ fields[2].getText() +"', ExpiryDate = '"+ dateFormat.format(exDate.getDate()) +"', Last4Digits = "+ Integer.parseInt(fields[3].getText()) +", CVV = "+ Integer.parseInt(CVV.getText()) +"\n" +
+                            "WHERE CustomerCustomer_ID = "+ custID +";";
                     con.Execute(sql);
                 }
+                remove(tPane);
+                table = new JTable(data, colHeads);
+                tPane = new JScrollPane(table);
+                table.setSize(900, 500);
+                add(tPane, BorderLayout.CENTER);
+                setVisible(true);
             }
         });
 
